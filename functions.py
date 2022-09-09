ab1539a3a1bdcfe1c782c41b8ec7bd7ea2180832
johnathanwastaken <danielortiz2001@hotmail.com>
2022-09-08 22:29:23 -0700

    Function for eat_lunch

--- functions.py
+++ functions.py
@@ -11,3 +11,17 @@ def pack(a,b,c):
 # A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __" 
 # (the first element of the list), and "Next I eat ___" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!". 
 # The function should not return anything.
+
+def eat_lunch(parm):
+  if len(parm) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(parm)):
+      if i == 0:
+        print(f"First I eat {parm[0]}")
+      else:
+        print(f"Next I eat {parm[i]}")
+
+# Test that your file works by running it in your terminal. Remember, you need to call your
+# functions in order for them to run. Make sure that all three functions run (please print the output
+# of pack()) before submitting the file.
