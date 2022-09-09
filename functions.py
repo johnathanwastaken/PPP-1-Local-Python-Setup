9479e9e81af3aadfe51e51cc05b54ef0a3826177
johnathanwastaken <danielortiz2001@hotmail.com>
2022-09-08 21:56:07 -0700

    Function for hello()

---
+++ functions.py
@@ -0,0 +1,8 @@
+# A function named hello() that prints a greeting to the user. This function should accept no arguments and returns nothing.
+
+def hello():
+  print("Hello, user!")
+
+# A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __" 
+# (the first element of the list), and "Next I eat ___" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!". 
+# The function should not return anything.
