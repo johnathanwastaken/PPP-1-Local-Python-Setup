afaab2538545ebfda21fb2c28b6649955cdba46e
johnathanwastaken <danielortiz2001@hotmail.com>
2022-09-08 21:57:31 -0700

    Function Instructions for pack()

--- functions.py
+++ functions.py
@@ -3,6 +3,9 @@
 def hello():
   print("Hello, user!")
 
+# A function named pack() that accepts three arguments, and returns a single list with the three arguments inside as list elements.
+
+
 # A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __" 
 # (the first element of the list), and "Next I eat ___" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!". 
 # The function should not return anything.
